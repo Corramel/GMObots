bfdcfb1b086af9c9d52f5eb809e4e8e9c4823a33
John smith <cutelestiaisdebestia@hotmail.com>
2015-12-27 03:26:38 -0500

    Update gamebot.js

--- gamebot.js
+++ gamebot.js
@@ -1,28 +1,17 @@
 var Discord = require('discord.js');
-var rl = require('readline');
 
 var client = new Discord.Client();
 
-// module.exports = 
-function ask(question, callback) {
-  var r = rl.createInterface({
-    input: process.stdin,
-    output: process.stdout});
-  r.question(question + '\n', function(answer) {
-    r.close();
-    callback(null, answer);
-  });
-}
-
-ask('Did you find this usefull?', function(answer) {
-  var playingChoice = answer
-  console.log(answer)
-});
-
 client.on("message", function(message){
-  if(message.content.startsWith(`!#memes`)){
-  client.setStatus('online', playingChoice)
+  if(message.author.id === "81526338728501248")){
+  client.setStatus('online', message.content)
   return;
 }
+});
+client.on("message", function(message){
+  if(message.content === "#!stop"){
+    client.logoff
+    return;
+  }
 });
 client.login(process.argv[2], process.argv[3]).catch((e) => console.log(e));
