c90d4665472edea0bbb1fdd11eeb6c0f07df08fc
John smith <cutelestiaisdebestia@hotmail.com>
2015-12-27 02:29:59 -0500

    Update gamebot.js

--- gamebot.js
+++ gamebot.js
@@ -1,5 +1,12 @@
 var Discord = require('discord.js');
+var prompt = require('prompt.js');
+
 var client = new Discord.Client();
+prompt.start()
+prompt.get(['playing'], function(err, result){
+  var playingChoice = result.playing
+  return;
+});
 client.on("message", function(message){
   if(message.content.startsWith(`!#memes`)){
   client.setStatus('online', "with Uni 💕")
