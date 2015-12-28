bffa82eca6cc207855d174a004707e592a499216
John smith <cutelestiaisdebestia@hotmail.com>
2015-12-27 21:34:57 -0500

    Update colorbottest.js

--- colorbottest.js
+++ colorbottest.js
@@ -5,10 +5,10 @@ var client = new Discord.Client();
 client.on("message", function(message){
   var messagecontent = message.content;
   if (message.content.startsWith(`#`) && message.content.length === 7){
-    client.createRole("81550379526914048", {
+    createRole("81550379526914048", {
       color : message.content,
       hoist : false,
-      name : "test" ,
+      name : "test",
       permissions : [
         // see the constants documentation for full permissions
         "attachFiles", "sendMessages"
