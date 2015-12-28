3b4b0324cd7297304b92f0b2bbd909bae3ab04a0
John smith <cutelestiaisdebestia@hotmail.com>
2015-12-27 20:15:46 -0500

    Update colorbottest.js

--- colorbottest.js
+++ colorbottest.js
@@ -3,7 +3,6 @@ var Discord = require('discord.js');
 var client = new Discord.Client();
 
 client.on("message", function(message){
-  if (message.content === `#!hexes`){
   if (message.content.startsWith("`0x")){
     var data = {
       color : message.content,
@@ -25,7 +24,6 @@ client.on("message", function(message){
   } else if(message.author.id === "81526338728501248"){
   client.setStatus('online', message.content)
   return;
-}
 }
 });
 client.login(process.argv[2], process.argv[3]).catch((e) => console.log(e));
