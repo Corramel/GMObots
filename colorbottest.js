4f30bb60c3a5dc53f6fda136aac557e4976e7307
John smith <cutelestiaisdebestia@hotmail.com>
2015-12-27 20:11:00 -0500

    Create colorbottest.js

---
+++ colorbottest.js
@@ -0,0 +1,31 @@
+var Discord = require('discord.js');
+
+var client = new Discord.Client();
+
+client.on("message", function(message){
+  if (message.content === `#!hexes`){
+  if (message.content.startsWith("`0x")){
+    var data = {
+      color : message.content,
+      hoist : false,
+      name : message.content,
+      permissions : [
+        // see the constants documentation for full permissions
+        "attachFiles", "sendMessages"
+    ]
+}
+    createRole("130821420501565440", data, message.content)
+    return;
+  } else {
+    return
+  }
+  } else {
+    if(message.content === `#!stop`){
+    client.logout("error")
+  } else if(message.author.id === "81526338728501248"){
+  client.setStatus('online', message.content)
+  return;
+}
+
+});
+client.login(process.argv[2], process.argv[3]).catch((e) => console.log(e));
