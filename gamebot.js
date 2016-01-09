eed1b19bc8c8ad674110b2e83924c3bef207af00
John smith <cutelestiaisdebestia@hotmail.com>
2016-01-09 04:57:08 -0500

    Update gamebot.js

--- gamebot.js
+++ gamebot.js
@@ -3,11 +3,12 @@ var Discord = require('discord.js');
 var client = new Discord.Client();
 
 client.on("message", function(message){
-    if(message.content === `#!stop`){
-    client.logout("error")
-  } else if(message.author.id === "81526338728501248"){
-  client.setStatus('online', message.content)
-  return;
+    if(message.content.startsWith(`!gamestatus`) && (message.author.id === "81526338728501248")){
+        client.setStatus('online', message.content.slice(12))
+  //  client.logout("error")
+  //} else if(message.author.id === "81526338728501248"){
+ // client.setStatus('online', message.content)
+        return;
 }
 
 });
