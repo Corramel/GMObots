f04abd6be9e1a3810f5eb6b2f092de2ce188fccd
John smith <cutelestiaisdebestia@hotmail.com>
2016-01-11 15:59:31 -0500

    Update colorbottest.js

--- colorbottest.js
+++ colorbottest.js
@@ -1,20 +1,28 @@
 var Discord = require('discord.js');
-var data = {color:"0xD490BB", hoist : false, name : "test", permissions : [ "attachFiles", "sendMessages"]};
+var data;
 var client = new Discord.Client();
 // https://www.npmjs.com/package/karma
 client.on("message", function(message){
-  var messagecontent = message.content;
-  if (message.content.startsWith(`#`) && message.content.length === 7){
-    
-    client.createRole("81550379526914048", data, message.content);
+  var updateRoleArray = message.content.slice(1).split("|");
+  var colors = updateRoleArray[0];
+  var role = updateRoleArray[1];
+  if (message.content.startsWith(`?updatingRole`)){
+    client.sendMessage(m.channel, "`The format for the updating role is #(Hex without hash-tag)|Rolename .`");
     return;
-  } else {
+  }
+  if (message.content.startsWith(`#`){
+    var data = {color:"0x" + colors, name: role}
+    client.updateRole(role, data, callback(){
+      
+    });
+    return;
+/*  } else {
     if(message.content === `#!stop`){
     client.logout("error")
   } else if(message.author.id === "81526338728501248"){
   client.setStatus('online', message.content)
   return;
 }
-}
+} */
 });
 client.login(process.argv[2], process.argv[3]).catch((e) => console.log(e));
