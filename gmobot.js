c22cc6a7b6f38e56d2291de6866497591b408032
John smith <cutelestiaisdebestia@hotmail.com>
2016-01-31 19:28:39 -0500

    Update gmobot.js

--- gmobot.js
+++ gmobot.js
@@ -64,7 +64,8 @@ client.on('message', function(message){
   
   if(message.content.toLowerCase().startsWith('what is the best GMO food?') || message.content.toLowerCase().startsWith('?gimo')){
     client.sendMessage(message, "Corn!")
-    var meme = ytdl(`https://www.youtube.com/watch?v=P1j5cVj5miA`, options = { filter: (format) => format.container === 'mp3', quality: 'lowest'});
+   // var meme = ytdl(`https://www.youtube.com/watch?v=P1j5cVj5miA`, options = { filter: (format) => format.container === 'mp3', quality: 'lowest'});
+    var meme = `https://www.youtube.com/watch?v=P1j5cVj5miA`
     if (client.internal.voiceConnection) {
         var connection = client.internal.voiceConnection;
         currentStream = YoutubeStream.getStream(meme);
