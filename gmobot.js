d7d0918e7ddee7da2187bf8c776bdc33fca2862f
John smith <cutelestiaisdebestia@hotmail.com>
2016-02-17 15:24:05 -0500

    Update gmobot.js

--- gmobot.js
+++ gmobot.js
@@ -20,9 +20,16 @@ var comparsionArray = [];
 var userMessage;
 var indexArray = [];
 var kek;
+var regularresponse;
 var similarities = 0;
 var lengthOfIndex;
 var questionArray = [];
+var exampleArray = ["Golden Rice is also an example of a GME(me). Golden rice, which isn’t a naughty pun, has been modified in a way to have Vitamin A in it. Since most asian countries that primarily produced rice would have deficiency of that vitamin. "
+, "Tomatoes ! Like potatoes but not with a P and has a T. Tomatoes are a prime example of GME(me)s since they have been genetically modified with a fish’s gene, aka a salmons gene, aka a cold water fish’s gene, to be more resistant to the cold. I guess you could say the tomatoes are a bit fishy."
+`🌽! The corn we see is a benefit of GMOs; they produce more kernels, use less water, and have become resistant to bacteria and herbicides. However corny they might seem, these corn are bigger and badder than your typical corn. Do you want this puny piece of crap: http://www.chapala.com/chapala/magnifecentmexico/maiz/maiz_fullblock.jpg ?
+Or do you want this big, bad, yellow piece of grainy goodness: http://images.wisegeek.com/ear-of-corn.jpg ? This corn has done more than gone to the hair salon to get that new do, this corn went to the lab and got some genes that help boost its resistance to bacteria and herbicides, and is more efficient with the water usage (I love a corn that learns to save money), but that doesn’t matter. Just look at the color and amount of kernels this corn has! Gee em oh <3! I think I’ve fallen for 🌽!
+`];
+
 function findInArray(letter) {
 kek = questionArray.indexOf(letter)
   if (kek in indexArray) {
@@ -76,9 +83,11 @@ function checkIfSimilar(array) {
   } else {
     return "I don't know what you meant by that."
   }
+  indexArray = [];
     }
-
+var command;
 var explainGMOArray = ['?gmo?', 'what is a gmo', 'what exactly is a gmo', 'what are gmos'];
+var commandArrays = [`?gmo?`, `?gimo`, `?benefits`, `?risks`, `?controversy`];
 var possibleGMO = ['g', 'm', 'o', ];
 var explainationHistory = [];
 var playQueue = [];
@@ -93,12 +102,6 @@ var botMention = false;
 var shouldStockpile = false;
 var stockpile = '';
 var url;
-
-// objects
-var messages = {
-  "?gmos?" : "what are GMOs",
-  "?gimos" : "Corn"
-};
 
 client.on('message', function(message){
   if (client.user.id === message.author.id) return;
@@ -138,7 +141,9 @@ client.on('message', function(message){
   if (!message.channel.equals(boundChannel)) return;
   var explainationHistory = [];
   if(message.content.toLowerCase().startsWith('what is the best genetically modified food?') || message.content.toLowerCase().startsWith('?gimo')){
-    client.reply(message, "Corn!")
+    regularresponse = "Corn!"
+    command = commandArray[1]
+    client.reply(message, regularresponse)
    // var meme = ytdl(`https://www.youtube.com/watch?v=P1j5cVj5miA`, options = { filter: (format) => format.container === 'mp3', quality: 'lowest'});
     //var meme = `https://www.youtube.com/watch?v=P1j5cVj5miA`
    // if (client.internal.voiceConnection) {
@@ -163,82 +168,12 @@ client.on('message', function(message){
       client.reply(message, `A GMO (genetically modified organism) is the result of a laboratory process where genes from the DNA of one species are extracted and artificially forced into the genes of an unrelated plant or animal. The foreign genes may come from bacteria, viruses, insects, animals or even humans. Because this involves the transfer of genes, GMOs are also known as "transgenic" organisms.`)
     }
   }
-  
-//  if (message.content.toLowerCase().startsWith("?")) {
-    
-//  }
- /* if (m.content.startsWith(`${botMention} y`) // youtube
-    || m.content.startsWith(`${botMention} q`) // queue
-    || m.content.startsWith(`${botMention} p`)) { // play
-
-    if (!checkCommand(m, 'yt')) return;
-
-    var vidList = spliceArguments(m.content)[1];
-
-    var vids = vidList.split(',');
-    var suppress = 0;
-    vids.forEach((vid, idx) => {
-      if (idx == 1) suppress = vids.length - 2;
-      if (idx == 2) suppress = -1;
-      parseVidAndQueue(vid, m, suppress);
-    });
-  }
-*/
-/*function parseVidAndQueue(vid, m, suppress) {
-  vid = resolveVid(vid, m);
-  if (!vid) {
-    client.reply(m, 'You need to specify a video!');
-    return;
-  }
-
-  getInfoAndQueue(vid, m, suppress);
-}
-
-
-function getInfoAndQueue(vid, m, suppress) {
-  requestUrl = 'http://www.youtube.com/watch?v=' + vid;
-  ytdl.getInfo(requestUrl, (err, info) => {
-    if (err) handleYTError(err);
-    else {
-      info.vid = vid;
-      info.obtainedFromGetInfo = true;
-      possiblyQueue(info, m.author.id, m, suppress);
-    }
-  });
-}
-*/
-/*function handleYTError(err) {
-  if (err.toString().indexOf('Code 150') > -1) {
-  // Video unavailable in country
-   boundChannel.sendMessage('This video is unavailable in the country the bot is running in! Please try a different video.');
-  } else if (err.message == 'Could not extract signature deciphering actions') {
-    boundChannel.sendMessage('YouTube streams have changed their formats, please update `ytdl-core` to account for the change!');
-  } else if (err.message == 'status code 404') {
-    boundChannel.sendMessage('That video does not exist!');
-  } else {
-    boundChannel.sendMessage('An error occurred while getting video information! Please try a different video.');
-  }
-
-  console.log(err.toString());
-} */
 function spliceArguments(message, after) {
   after = after || 2;
   var rest = message.split(' ');
   var removed = rest.splice(0, after);
   return [removed.join(' '), rest.join(' ')];
 }
-/*
-function play(vid2beplayed) {
-  currentVideo = vid2beplayed;
-  if (client.internal.voiceConnection) {
-    var connection = client.internal.voiceConnection;
-    currentStream = YoutubeStream.getStream(video2beplayed);
-
-    currentStream.on('error', (err) => {
-      boundChannel.sendMessage(`There was an error during playback, error: **${err}**`);
-    });
-}
-} */
 }); 
 client.login(process.argv[2], process.argv[3]).catch((e) => console.log(e));
 //Utilize save.js next time to get the job done, make the "commands" specifically made for it, but make the "yt" command run by itself, so a mixture of both, then.
